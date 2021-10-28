b979f9c4962df56674af0e7f6d0b939dfbaa6b58
dnamrata <namrata@railofy.com>
2021-10-28 12:31:53 +0530

    basic file structure for a simple app done

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useContext } from "react";
+// import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  console.log("rendering index with context");
+  return <div></div>;
+};
+
+export default Home;
